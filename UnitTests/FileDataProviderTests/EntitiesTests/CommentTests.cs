d252711843efd91676cc9faca96354902ff70be8
Dimitar Tonchev <princerollyne@gmail.com>
2017-03-30 12:38:46 +0300

    Completed Entity Unit tests

---
+++ UnitTests/FileDataProviderTests/EntitiesTests/CommentTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using FileDataProvider.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.FileDataProviderTests.EntitiesTests
+{
+    [TestClass]
+    public class CommentTests
+    {
+        [TestMethod]
+        public void IsTheIdSetProperly()
+        {
+            //Arrange
+            var item = new Comment();
+            const int idTester = 56;
+
+            //Act
+            item.Id = idTester;
+
+            //Assert
+            Assert.AreEqual(idTester, item.Id);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DoesPropertyThrowArgumentExceptionWhenNegativeIdIsSet()
+        {
+            //Arrange
+            var item = new Comment();
+            int idTester = -56;
+
+            //Act
+            item.Id = idTester;
+        }
+
+        [TestMethod]
+        public void IsTheTaskIdSetProperly()
+        {
+            //Arrange
+            var item = new Comment();
+            const int idTester = 56;
+
+            //Act
+            item.TaskId = idTester;
+
+            //Assert
+            Assert.AreEqual(idTester, item.TaskId);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DoesPropertyThrowArgumentExceptionWhenNegativeTaskIdIsSet()
+        {
+            //Arrange
+            var item = new Comment();
+            int idTester = -56;
+
+            //Act
+            item.TaskId = idTester;
+        }
+
+        [TestMethod]
+        public void IsTheAuthorIdSetProperly()
+        {
+            //Arrange
+            var item = new Comment();
+            const int idTester = 56;
+
+            //Act
+            item.AuthorId = idTester;
+
+            //Assert
+            Assert.AreEqual(idTester, item.AuthorId);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DoesPropertyThrowArgumentExceptionWhenNegativeAuthorIdIsSet()
+        {
+            //Arrange
+            var item = new Comment();
+            int idTester = -56;
+
+            //Act
+            item.AuthorId = idTester;
+        }
+
+        [TestMethod]
+        public void IsTheBodySetProperly()
+        {
+            //Arrange
+            var item = new Comment();
+            const string bodyTester = "BSoD is often caused by bad RAM sticks.";
+
+            //Act
+            item.Body = bodyTester;
+
+            //Assert
+            Assert.AreEqual(bodyTester, item.Body);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DoesPropertyThrowArgumentExceptionWhenEmptyBodyStringIsSet()
+        {
+            //Arrange
+            var item = new Comment();
+            var bodyTester = string.Empty;
+
+            //Act
+            item.Body = bodyTester;
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DoesPropertyThrowArgumentExceptionWhenNullIsSet()
+        {
+            //Arrange
+            var item = new Comment();
+            string bodyTester = null;
+
+            //Act
+            item.Body = bodyTester;
+        }
+
+    }
+}
---
+++ UnitTests/FileDataProviderTests/EntitiesTests/TaskTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using FileDataProvider.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.FileDataProviderTests.EntitiesTests
+{
+    [TestClass]
+    public class TaskTests
+    {
+        [TestMethod]
+        public void IsTheIdSetProperly()
+        {
+            //Arrange
+            var item = new Task();
+            const int idTester = 56;
+
+            //Act
+            item.Id = idTester;
+
+            //Assert
+            Assert.AreEqual(idTester, item.Id);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DoesPropertyThrowArgumentExceptionWhenNegativeIdIsSet()
+        {
+            //Arrange
+            var item = new Task();
+            int idTester = -56;
+
+            //Act
+            item.Id = idTester;
+        }
+
+        [TestMethod]
+        public void IsTheHeaderSetProperly()
+        {
+            //Arrange
+            var item = new Task();
+            string headerTester = "RAM Diagnostics Tool";
+
+            //Act
+            item.Header = headerTester;
+
+            //Assert
+            Assert.AreEqual(headerTester, item.Header);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DoesPropertyThrowArgumentExceptionWhenEmptyHeaderStringIsSet()
+        {
+            //Arrange
+            var item = new Task();
+            string headerTester = string.Empty;
+
+            //Act
+            item.Header = headerTester;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DoesPropertyThrowArgumentExceptionWhenNullUsernameIsSet()
+        {
+            //Arrange
+            var item = new Task();
+            const string headerTester = null;
+
+            //Act
+            item.Header = headerTester;
+        }
+        [TestMethod]
+        public void IsTheRequiredHoursSetProperly()
+        {
+            //Arrange
+            var item = new Task();
+            const int hoursTester = 4;
+
+            //Act
+            item.RequiredHours = hoursTester;
+
+            //Assert
+            Assert.AreEqual(hoursTester, item.RequiredHours);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DoesPropertyThrowArgumentExceptionWhenNegativeRequiredHoursIsSet()
+        {
+            //Arrange
+            var item = new Task();
+            const int hoursTester = -4;
+
+            //Act
+            item.RequiredHours = hoursTester;
+        }
+        [TestMethod]
+        public void IsTheExecutitiveIdSetProperly()
+        {
+            //Arrange
+            var item = new Task();
+            const int idTester = 56;
+
+            //Act
+            item.ExecutitiveId = idTester;
+
+            //Assert
+            Assert.AreEqual(idTester, item.ExecutitiveId);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DoesPropertyThrowArgumentExceptionWhenNegativeExecutitiveIdIsSet()
+        {
+            //Arrange
+            var item = new Task();
+            int idTester = -56;
+
+            //Act
+            item.ExecutitiveId = idTester;
+        }
+        [TestMethod]
+        public void IsTheCreatorIdSetProperly()
+        {
+            //Arrange
+            var item = new Task();
+            const int idTester = 56;
+
+            //Act
+            item.CreatorId = idTester;
+
+            //Assert
+            Assert.AreEqual(idTester, item.CreatorId);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DoesPropertyThrowArgumentExceptionWhenNegativeCreatorIdIsSet()
+        {
+            //Arrange
+            var item = new Task();
+            int idTester = -56;
+
+            //Act
+            item.CreatorId = idTester;
+        }
+    }
+}
