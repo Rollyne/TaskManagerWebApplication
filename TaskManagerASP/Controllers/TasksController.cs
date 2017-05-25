d1072ad91e050c2fe9aa9bb27f62252d7542011f
Rollyne <rollyne@abv.bg>
2017-05-25 16:16:42 +0300

    Added searching and paging functionallity

--- TaskManagerASP/Controllers/TasksController.cs
+++ TaskManagerASP/Controllers/TasksController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Data.Entities.Entities;
 using Data.Entities.Repositories;
-using DbDataProvider;
+using Microsoft.AspNetCore.Http;
 using TaskManagerASP.Models;
 using TaskManagerASP.Tools;
 
@@ -28,6 +30,57 @@ namespace TaskManagerASP.Controllers
             ViewData["ErrorMessage"] = ErrorMessages.NoAccess("task");
             return false;
            
+        }
+
+        [HttpGet]
+        public IActionResult Index( int? itemsPerPage, string search = null, bool description = false, bool header = false, int page = 1)
+        {
+            if (!IsAuthorized())
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int itemsAmount;
+            if (itemsPerPage != null)
+            {
+                this.HttpContext.Session.SetInt32("itemsPerPage", itemsPerPage.Value);
+                itemsAmount = itemsPerPage.Value;
+            }
+            else
+            {
+                itemsAmount = this.HttpContext.Session.GetInt32("itemsPerPage") ?? Constants.DefaultItemsPerPage;
+            }
+
+            ICollection<Task> items = 
+                Repository.GetAll(AuthenticationManager.GetLoggedUser(HttpContext).Id);
+            ICollection<Task> result = new List<Task>();
+            if (header || description)
+            {
+                if (header)
+                    result = items.Where(i => ((i.Header.StartsWith(search ?? "") || string.IsNullOrEmpty(search)))).ToList();
+                if (description)
+                    foreach (
+                        var item in
+                        items.Where(i => (i.Description.StartsWith(search ?? "") || string.IsNullOrEmpty(search))))
+                    {
+                        result.Add(item);
+                    }
+            }
+            else
+            {
+                result = items;
+            }
+                        
+
+            ViewData["PagesAvaliable"] = (int)Math.Ceiling((double)result.Count / itemsAmount);
+
+            result = result.Skip((page - 1) * itemsAmount)
+                .Take(itemsAmount)
+                .ToList();
+
+            ViewData[$"Tasks"] = result;
+            ViewData["CurrentPage"] = page;
+            
+            return View();
         }
 
         [HttpGet]
